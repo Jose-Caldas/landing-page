6539a700dac5e829e92049ef8d50fb11bb3d8966
José Caldas <jrcaldas@yahoo.com.br>
2021-06-21 17:25:44 -0300

    make dynamic sectionAboutProject

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -20,12 +20,26 @@ fragment header on LandingPage {
     }
   }
 }
+
+fragment sectionAboutProject on LandingPage{
+  sectionAboutProject{
+    title
+    description
+    image{
+      alternativeText
+      url
+    }
+  }
+}
+
 query GET_LANDING_PAGE {
   landingPage {
     ...logo
     ...header
+    ...sectionAboutProject
   }
 }
+
 
 `
 export default GET_LANDING_PAGE
